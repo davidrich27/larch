e8124f11a07d5b469d60728a2024ced98ae54ca2
David Rich <david.rich27@gmail.com>
2024-12-05 12:10:22 -0800

    fixes to conda-recipes

--- conda-recipes/build.sh
+++ conda-recipes/build.sh
@@ -11,6 +11,9 @@ MAKE_NUM_THREADS=${MAKE_NUM_THREADS:-"20"}
 LARCH_INCLUDE_TEST=${LARCH_INCLUDE_TEST:-"false"}
 LARCH_RUN_TEST=${LARCH_RUN_TEST:-"false"}
 
+# INSTALL_PATH=${PREFIX}
+INSTALL_PATH=$(pwd)/install
+
 echo "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}"
 echo "CMAKE_USE_USHER: ${CMAKE_USE_USHER}"
 echo "CMAKE_NUM_THREADS: ${CMAKE_NUM_THREADS}"
@@ -18,9 +21,10 @@ echo "MAKE_NUM_THREADS: ${MAKE_NUM_THREADS}"
 echo "LARCH_INCLUDE_TEST: ${LARCH_INCLUDE_TEST}"
 echo "LARCH_RUN_TEST: ${LARCH_RUN_TEST}"
 echo "PREFIX: ${PREFIX}"
+echo "INSTALL_PATH: ${INSTALL_PATH}"
 
 export CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS}
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} ..
 make -j${MAKE_NUM_THREADS}
 make install
 
@@ -32,6 +36,13 @@ if [[ ${LARCH_RUN_TEST} == true ]]; then
     cd ../
 fi
 
-# cd install
-# cp bin/* $PREFIX/bin
-# cp -r lib/larch $PREFIX/lib
+cd $INSTALL_PATH
+cp bin/* ${PREFIX}/bin
+cp -r lib/* ${PREFIX}/lib
+cd ../
+
+echo "CHECK VERSION..."
+cd ~
+larch-usher --version
+larch-dagutil --version
+larch-dag2dot --version
